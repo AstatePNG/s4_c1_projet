cb91aa5d4233fb7e7d36f04c677a474d1bd0a917
Elsa Hamon <elsa.hamon@etu.unicaen.fr>
2025-02-12 15:35:17 +0100

    avant le sucre syntaxique

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,79 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+
+let text = ref(""); // ref permet à la variable de se mettre automatiquement à jour dans le HTML
+
+function updateText(event) {
+  text.value = event.target.value;
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div class="container">
+      <form class="card">
+        <textarea
+          name="post"
+          id="post"
+          placeholder="Quelles sont les dernières nouvelles mon brave ?"
+          v-bind:value="text"
+          v-on:input="updateText"
+        >
+          <!-- v-bind permet la liaison entre la valeur de la variable js et la variable html, de js vers html -->
+          <!-- input appelle une fonction à chaque input (observateur), cela permet la liaison de html vers js -->
+          <!-- pour que la chaine de caractères soit considérer comme un nom js, on lui ajoute v-on -->
+        </textarea>
+        <button type="submit">>>></button>
+      </form>
+
+      <p>
+        {{ text }}
+        <!-- les doubles accolades indiquent l'utilisation de la valeur de la variable js "text"-->
+      </p>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  height: 100vh;
+  margin: 0 auto;
+  max-width: 640px;
+}
+.card {
+  background-color: var(--color-bg-secondary);
+  border-radius: 10px;
+  border: 1px solid var(--color-border);
+  margin-bottom: 1rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+form {
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+  padding: 1rem 1.5rem;
+  width: 100%;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+textarea {
+  background: none;
+  border: none;
+  color: var(--color-text-primary);
+  flex: 1;
+  margin-bottom: 1rem;
+  outline: none;
+  padding: 0.5rem 0;
+  resize: none;
+  field-sizing: content;
+}
+button {
+  align-self: flex-end;
+  background: none;
+  border-radius: 10px;
+  border: 1px solid var(--color-border);
+  color: var(--color-text-primary);
+  cursor: pointer;
+  font-size: 1rem;
+  height: 40px;
+  padding: 0 1rem;
 }
 </style>
